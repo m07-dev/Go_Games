c198e3b00584a3c07d872ba2d98a6fc4983a0d31
bachir-tahenni <tahennibachir9@gmail.com>
2026-01-06 05:41:02 +0100

    Update launcher.java

--- src/launcher.java
+++ src/launcher.java
@@ -32,29 +32,33 @@ public class launcher {
             System.out.println("                                                                        Bienvenue dans GoGames : appuyez sur 1 pour lancer une partie de GO ");
             BoutonLancer = sc.nextInt();
         } while (BoutonLancer != 1);
+
+        // demande du pseudo du joueur 1
         System.out.println("Entrez le prénom du Joueur 1 (Blancs) :");
         String nomJ1 = sc.next();
 
+        // demande du pseudo du joueur 2
         System.out.println("Entrez le prénom du Joueur 2 (Noirs) :");
         String nomJ2 = sc.next();
+
         // demande la taille du plateau sur lequel le joueur veut jouer
         do {
             System.out.println("Sur un plateau de quelle taille voulez vous jouez : 9, 13, 19 ");
             taille = sc.nextInt();
         } while (taille != 9 && taille != 13 && taille != 19);
 
-        System.out.println("Joueur numero 1 vous avez les pierres blanches vous debutez avec un avantage de 0.5 points\n" +
-                "Joueur numero 2 vous avez les pierres noires c'est donc vous qui commencez ");
+        System.out.println(nomJ1 + " vous avez les pierres blanches vous debutez avec un avantage de 0.5 points\n" +
+                nomJ2+" vous avez les pierres noires c'est donc vous qui commencez ");
         System.out.println();
 
         // Presentation du plateau de jeu
         System.out.println("Voici le plateau sur le quelle vous allez jouer ");
         System.out.println();
 
-        // le tableau d'entiers
+        // Creation du plateau de jeu
         int[][] Goban = MethodePlateau.creationGoban(taille);
 
-       // on affiche
+        // Affihchage du plateau de jeu
         Affichage.AffichageGoban(Goban);
 
         System.out.println();
